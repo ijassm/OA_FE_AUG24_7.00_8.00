e35abe6d47678f7a2c9976319a4e08759077f504
ijassm <ijass.ocean@gmail.com>
2024-11-18 20:09:39 +0530

    Completed datatype & variables

---
+++ JS/intro.js
@@ -0,0 +1,67 @@
+// console.log("Hello JS");
+// document.write("<h2>Hello JS</h2>");
+// alert("Hello JS");
+
+// Basic Datatypes
+
+// console.log("Hello JS");
+// console.log(typeof "Hello JS");
+// console.log('Hello JS');
+// console.log(typeof 'Hello JS');
+// console.log(`Hello JS ${5 + 5}`);
+// console.log(typeof `Hello JS`);
+
+// console.log(1);
+// console.log(typeof 1);
+
+// console.log(1.5);
+// console.log(typeof 1.5);
+
+// console.log(1234n);
+// console.log(typeof 1234n);
+
+// console.log(Symbol("abc"));
+// console.log(typeof Symbol("abc"));
+
+// console.log(true);
+// console.log(typeof true);
+
+// console.log(false);
+// console.log(typeof false);
+
+// console.log(undefined);
+// console.log(typeof undefined);
+
+// console.log(null);
+// console.log(typeof null);
+
+// console.log([]);
+// console.log(typeof []);
+
+// console.log({});
+// console.log(typeof {});
+
+// console.log("hello".toUpperCase());
+// console.log(new String("hello").toUpperCase());
+
+// var a,
+//   b = 20,
+//   c = 10;
+
+// console.log(a);
+
+// a = 10;
+// console.log(a);
+// a = 12.5;
+// console.log(a);
+// a = true;
+// console.log(a);
+// a = [];
+// console.log(a);
+// a = {};
+// console.log(a);
+// a = "hello";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
