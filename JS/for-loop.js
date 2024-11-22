b946ce4239d53f542e9f19a89ec9fde6bfe35e8d
ijassm <ijass.ocean@gmail.com>
2024-11-22 20:30:31 +0530

    Completed for loop

---
+++ JS/for-loop.js
@@ -0,0 +1,27 @@
+// var a = "Hello";
+// var b = "world";
+
+// console.log(a + " " + b);
+// console.log(`${a} ${b}`);
+// console.log(document);
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+for (var i = 1; i < 2; i++) {
+  for (var j = 1; j <= 1; j++) {
+    for (let k = 0; k < 1; k++) {
+      for (let l = 0; l < 1; l++) {
+        console.log(i, j, k, l);
+      }
+    }
+  }
+}
+
+
+// i = 1
+        // j = 1 
+                // k = 0
+        // j = 2
+                // k = 0
