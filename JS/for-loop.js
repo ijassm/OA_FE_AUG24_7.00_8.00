b713a1be1659bf9b5bbf8cf3d67b3bfce15de7c3
ijassm <ijass.ocean@gmail.com>
2024-11-25 20:24:41 +0530

    Completed var | let | const

--- JS/for-loop.js
+++ JS/for-loop.js
@@ -9,19 +9,18 @@
 //   console.log(i);
 // }
 
-for (var i = 1; i < 2; i++) {
-  for (var j = 1; j <= 1; j++) {
-    for (let k = 0; k < 1; k++) {
-      for (let l = 0; l < 1; l++) {
-        console.log(i, j, k, l);
-      }
-    }
-  }
-}
-
+// for (var i = 1; i < 2; i++) {
+//   for (var j = 1; j <= 1; j++) {
+//     for (let k = 0; k < 1; k++) {
+//       for (let l = 0; l < 1; l++) {
+//         console.log(i, j, k, l);
+//       }
+//     }
+//   }
+// }
 
 // i = 1
-        // j = 1 
-                // k = 0
-        // j = 2
-                // k = 0
+// j = 1
+// k = 0
+// j = 2
+// k = 0
---
+++ JS/keyword.js
@@ -0,0 +1,26 @@
+// function func() {
+//   var a = "hi";
+//   console.log(a);
+// }
+
+// console.log(a, "accessing before declaration");
+
+// {
+//   var a = "inside block";
+//   console.log(a);
+// }
+
+// var a = "outside block";
+// b = "outside block";
+// // window.b = "outside block";
+
+// func();
+
+// console.log(a);
+// console.log(window.a);
+// console.log(b);
+// console.log(window.b);
+
+// console.log(a);
+
+// var a = "hello";
