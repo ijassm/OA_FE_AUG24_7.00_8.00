6281853ec98875d3dc313cb0dd23eddd7fca89f4
ijassm <ijass.ocean@gmail.com>
2024-11-27 20:10:36 +0530

    Completed type-casting & while

---
+++ JS/type-casting.js
@@ -0,0 +1,11 @@
+let a = "123abc";
+
+console.log(typeof a);
+// console.log(typeof Number(a), Number(a));
+// console.log(typeof parseFloat(a), parseFloat(a));
+console.log(typeof new Array("Hello"), new Array("Hello"));
+// console.log(5 - "5634");
+// console.log("5634" - 5);
+// console.log("5634" * 5);
+// console.log("ABC" - 5);
+console.log(typeof parseInt(a), parseInt(a));
