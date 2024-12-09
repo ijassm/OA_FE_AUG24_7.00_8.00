b34ab7f33dc96a48264abc3a8ddfcdcaea622024
ijassm <ijass.ocean@gmail.com>
2024-12-09 20:18:09 +0530

    Completed array

---
+++ JS/Array.js
@@ -0,0 +1,37 @@
+// let array = new Array();
+let array = [1, 3, 5, 7, 8, 9, 5];
+
+// accessing array
+
+// console.log(array[0], "array[0]");
+// console.log(array[1], "array[1]");
+
+// console.log(array.at(-1), "array.at(-1)");
+// console.log(array.at(-2), "array.at(-2)");
+
+// array[0] = 10;
+// array[1] = 20;
+
+// console.log(array[0]); // Output: 10
+// console.log(array[1]); // Output: 20
+
+// array.push(1);
+// array.push(2);
+// array.pop();
+
+// console.log(array);
+// console.log(typeof array);
+
+// iterate
+
+// ✔
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// ✔
+// for (let i of array) console.log(i);
+
+// ❌
+// Array.prototype.something = "something";
+// for (let i in array) console.log(i);
